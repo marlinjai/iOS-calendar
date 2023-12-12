9d9aeee2734a5bd8c8bd1feffc53f59686608208
Marlin Jai <marlinjp@icloud.com>
2023-12-12 08:48:45 +0100

    Initial Commit

---
+++ iOS5 Pohl/iOS5_PohlApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS5_PohlApp.swift
+//  iOS5 Pohl
+//
+//  Created by Marlin Pohl on 12.12.23.
+//
+
+import SwiftUI
+
+@main
+struct iOS5_PohlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iOS5 PohlUITests/iOS5_PohlUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  iOS5_PohlUITestsLaunchTests.swift
+//  iOS5 PohlUITests
+//
+//  Created by Marlin Pohl on 12.12.23.
+//
+
+import XCTest
+
+final class iOS5_PohlUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
